f2b6aac29c2da66b7d0820a553959e68323a5db4
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2023-10-20 14:15:59 +0200

    Create s3_bookings.sql

---
+++ models/reporting/s3_bookings.sql
@@ -0,0 +1,27 @@
+{{ config (
+    alias = target.database + '_s3_bookings',
+    materialized='incremental',
+    unique_key='unique_key'
+)}}
+
+WITH last_record_date as (select 
+appointment_id, max(_fivetran_synced) as last_synced_date
+from {{ source('s3_raw','bookings_raw') }}
+group by appointment_id)
+
+, final_data as (select 
+    bookings_raw.appointment_id,
+    created_date,
+    location_name,
+    client_type,
+    updated_date,
+    status,
+    appointment_date,
+    "count" as appointments,
+    bookings_raw.appointment_id as unique_key
+from {{ source('s3_raw','bookings_raw') }}
+left join last_record_date 
+on bookings_raw._fivetran_synced = last_record_date.last_synced_date
+and bookings_raw.appointment_id = last_record_date.appointment_id)
+
+select * from final_data 
