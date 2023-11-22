e1a8299df5a4c6e6350facab1612a57766d90ac2
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2023-11-22 10:37:06 -0500

    Update blended_performance.sql

--- models/reporting/blended_performance.sql
+++ models/reporting/blended_performance.sql
@@ -6,7 +6,7 @@ SELECT 'Facebook - Prospecting' as channel, date, date_granularity, store_type, 
         COALESCE(SUM(spend),0) as spend, COALESCE(SUM(spend),0) as pros_spend, COALESCE(SUM(link_clicks),0) as clicks, 
         COALESCE(SUM(impressions),0) as impressions, COALESCE(SUM(purchases),0) as purchases, COALESCE(SUM(purchases),0) as pros_purchases, 
         COALESCE(SUM(bookings),0) as paid_bookings, 0 as bookings, 0 as new_bookings
-    FROM {{ ref('facebook_ad_performance') }}
+    FROM {{ source('reporting','facebook_ad_performance') }}
     WHERE campaign_type_default ~* 'Prospecting'
     GROUP BY 1,2,3,4,5
     
@@ -16,7 +16,7 @@ SELECT 'Facebook - Prospecting' as channel, date, date_granularity, store_type, 
         COALESCE(SUM(spend),0) as spend, 0 as pros_spend, COALESCE(SUM(link_clicks),0) as clicks, COALESCE(SUM(impressions),0) as impressions, 
         COALESCE(SUM(purchases),0) as purchases, 0 as pros_purchases, COALESCE(SUM(bookings),0) as paid_bookings, 
         0 as bookings, 0 as new_bookings
-    FROM {{ ref('facebook_ad_performance') }}
+    FROM {{ source('reporting','facebook_ad_performance') }}
     WHERE campaign_type_default ~* 'Retargeting'
     GROUP BY 1,2,3,4,5
     
@@ -26,7 +26,7 @@ SELECT 'Facebook - Prospecting' as channel, date, date_granularity, store_type, 
         COALESCE(SUM(spend),0) as spend, COALESCE(SUM(spend),0) as pros_spend, COALESCE(SUM(clicks),0) as clicks, 
         COALESCE(SUM(impressions),0) as impressions, COALESCE(SUM(purchases),0) as purchases, COALESCE(SUM(purchases),0) as pros_purchases, 
         COALESCE(SUM(bookings),0) as paid_bookings, 0 as bookings, 0 as new_bookings
-    FROM {{ ref('googleads_campaign_performance') }}
+    FROM {{ source('reporting','googleads_campaign_performance') }}
     WHERE campaign_type_default ~* 'Search Branded'
     GROUP BY 1,2,3,4,5
     
@@ -36,7 +36,7 @@ SELECT 'Facebook - Prospecting' as channel, date, date_granularity, store_type, 
         COALESCE(SUM(spend),0) as spend, 0 as pros_spend, COALESCE(SUM(clicks),0) as clicks, COALESCE(SUM(impressions),0) as impressions, 
         COALESCE(SUM(purchases),0) as purchases, 0 as pros_purchases, COALESCE(SUM(bookings),0) as paid_bookings, 
         0 as bookings, 0 as new_bookings
-    FROM {{ ref('googleads_campaign_performance') }}
+    FROM {{ source('reporting','googleads_campaign_performance') }}
     WHERE campaign_type_default ~* 'Search Nonbrand'
     GROUP BY 1,2,3,4,5
     
@@ -46,7 +46,7 @@ SELECT 'Facebook - Prospecting' as channel, date, date_granularity, store_type, 
         COALESCE(SUM(spend),0) as spend, COALESCE(SUM(spend),0) as pros_spend, COALESCE(SUM(clicks),0) as clicks, 
         COALESCE(SUM(impressions),0) as impressions, COALESCE(SUM(purchases),0) as purchases, COALESCE(SUM(purchases),0) as pros_purchases, 
         COALESCE(SUM(bookings),0) as paid_bookings, 0 as bookings, 0 as new_bookings
-    FROM {{ ref('tiktok_ad_performance') }}
+    FROM {{ source('reporting','tiktok_ad_performance') }}
     GROUP BY 1,2,3,4,5
     
     UNION ALL
@@ -54,5 +54,5 @@ SELECT 'Facebook - Prospecting' as channel, date, date_granularity, store_type, 
     SELECT 'Looker' as channel, date, date_granularity, store_type, market, 
         0 as spend, 0 as pros_spend, 0 as clicks, 0 as impressions, 0 as purchases, 0 as pros_purchases, 0 as paid_bookings, 
         COALESCE(SUM(bookings),0) as bookings, COALESCE(SUM(CASE WHEN client_type ~* 'New Client' THEN bookings END),0) as new_bookings
-    FROM {{ ref('looker_booking_performance') }}
+    FROM {{ source('reporting','looker_booking_performance') }}
     GROUP BY 1,2,3,4,5
